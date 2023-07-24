c3383347c96c01d38bf28b218210709084e9faef
Amy <amy.huang@lyncustek.com>
2023-07-24 07:50:18 +0000

    optimize flexibility

---
+++ todo-list/src/constants/index.js
@@ -0,0 +1,11 @@
+// const TO_DO_LIST_STORAGE_KEY = 'TO_DO_LIST'
+
+// ---------------------------------------
+// 1. const的value沒有限制naming rule
+// 2. 讓用的人可以再決定是否要命名alias
+
+// export default {
+//   TO_DO_LIST_STORAGE_KEY: TO_DO_LIST_STORAGE_KEY
+// }
+
+export * from './localStorageKeys'
